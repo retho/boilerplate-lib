c82bf3a6a207b1b80e2d8855b564b8aa5f837806
Evgeny Safonov <safonov.e.s@gmail.com>
2021-03-07 10:55:25 +0300

    .

--- sample/src/App.tsx
+++ sample/src/App.tsx
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
+import React, { FC } from "react";
 
-import ExampleComponent from "example-typescript-react-component-library";
+import ExampleComponent from "boilerplate-lib";
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <ExampleComponent text="Modern React component module" />
-            </div>
-        );
-    }
+const App: FC = () => {
+  return (
+    <div>
+      <ExampleComponent text="Modern React component module" />
+    </div>
+  );
 }
+
+export default App;
